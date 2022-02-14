8de477f10635d1cba16b7cc870dd38b84c0c66bf
mrridho01 <muhammadridho01@gmail.com>
2022-02-14 21:45:36 +0700

    Migration contract, photo, video file, drawing

---
+++ database/migrations/2022_02_14_142840_create_contracts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contracts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('project_id');
+            $table->longText('path');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contracts');
+    }
+};
