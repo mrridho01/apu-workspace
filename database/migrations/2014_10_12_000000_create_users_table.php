cb0cf0d249f56484da8aa9fef6b4dcc4d7ebf56d
mrridho01 <muhammadridho01@gmail.com>
2022-02-14 15:46:14 +0700

    Migration user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->longText('avatar');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
